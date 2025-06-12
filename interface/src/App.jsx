04c7302de1fe95d6b77426b74fd4f7533af892f7
aaayush2604 <aayushfabwani2@gmail.com>
2025-06-12 20:23:05 +0530

    Play Against AI Added

--- interface/src/App.jsx
+++ interface/src/App.jsx
@@ -1,21 +1,3 @@
-// import ChessGame from './components/ChessGame'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Lobby from './pages/Lobby';
-// import GameRoom from './pages/GameRoom';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Lobby />} />
-//         <Route path="/game/:gameId" element={<GameRoom />} />
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
-// export default App
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Lobby from './pages/Lobby';
 import GameRoom from './pages/GameRoom';
