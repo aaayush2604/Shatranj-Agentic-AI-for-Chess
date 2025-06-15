ee5ad08f9546db9edfc0262bcf80885b2bba778b
aaayush2604 <aayushfabwani2@gmail.com>
2025-06-15 14:26:37 +0530

    First FrontEnd for Lobby Added

--- interface/src/pages/Lobby.jsx
+++ interface/src/pages/Lobby.jsx
@@ -29,29 +29,32 @@ const Lobby = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4 text-center">
-      <h1 className="text-2xl font-bold mb-4">Chess Lobby</h1>
+    <div className="h-screen gap-4 text-center bg-[var(--color-two)]">
+      <div className='absolute h-[95%] w-4/5 right-10 bottom-0 flex flex-col justify-end items-start'>
+        <h1 className="absoute  text-5xl font-bold mb-4 font-toccoBold text-[var(--text-color-two)]">Chess Lobby</h1>
+        <div className="h-[80%] w-full rounded-t-2xl flex flex-col items-start justify-center gap-10 text-center bg-[var(--primary-color)] pl-7">
+          
+          <button onClick={handleCreateGame} className="px-4 py-2 w-[310px] h-[50px] text-start hover:border-4 hover:border-black rounded-xl font-arvoBold text-xl shadow-black hover:shadow-md">
+            Create Multiplayer Game
+          </button>
 
-      <button onClick={handleCreateGame} className="bg-blue-600 text-white px-4 py-2 rounded">
-        Create Multiplayer Game
-      </button>
+          <div>
+            <input
+              className="border-4 w-[160px] h-[50px] px-4 outline-none focus:ring-0 focus:border-none py-2 text-start rounded border-none font-arvoBold"
+              value={gameId}
+              onChange={(e) => setGameId(e.target.value)}
+              placeholder="Enter Game ID"
+            />
 
-      <input
-        className="border px-3 py-1 rounded"
-        value={gameId}
-        onChange={(e) => setGameId(e.target.value)}
-        placeholder="Enter Game ID"
-      />
-
-      <button onClick={handleJoinGame} className="bg-green-600 text-white px-4 py-2 rounded">
-        Join Multiplayer Game
-      </button>
-
-      <div className="my-4">OR</div>
-
-      <button onClick={handlePlayVsAI} className="bg-purple-600 text-white px-4 py-2 rounded">
-        Play vs AI
-      </button>
+            <button onClick={handleJoinGame} className="px-4 py-2 w-[310px] h-[50px] hover:border-4 hover:border-black rounded-xl font-arvoBold text-xl shadow-black hover:shadow-md text-start">
+              Join Multiplayer Game
+            </button>
+          </div>
+          <button onClick={handlePlayVsAI} className="px-4 py-2 w-[310px] h-[50px] hover:border-4 hover:border-black rounded-xl font-arvoBold text-xl shadow-black hover:shadow-md text-start">
+            Play vs AI
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
